112fb7183332ad999f9026ff84c52dda63dc8bfa
Saul Castillo <secch97@gmail.com>
2023-05-20 00:03:20 -0400

    Client side routing

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 
 const Root = () => {
   return (
@@ -33,10 +33,10 @@ const Root = () => {
             <nav>
                 <ul>
                     <li>
-                        <a href={"/contacts/1"}>Your Name</a>
+                        <Link to={"/contacts/1"}>Your Name</Link>
                     </li>
                     <li>
-                        <a href={"/contacts/2"}>Your Friend</a>
+                        <Link to={"/contacts/2"}>Your Friend</Link>
                     </li>
                 </ul>
             </nav>
