cd122578e63b13a4b5ec7993aca9a64b17b10903
Saul Castillo <secch97@gmail.com>
2023-05-24 22:04:34 -0400

    Added a search spinner

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -19,6 +19,8 @@ const Root = () => {
   const navigation = useNavigation();
   const submit = useSubmit();
 
+  const searching = navigation.location && new URLSearchParams(navigation.location.search).has("q")
+
   useEffect(()=>{
     document.getElementById("q").value = q;
   }, [q]);
@@ -31,6 +33,7 @@ const Root = () => {
           <Form id="search-form" role="search">
             <input
               id="q"
+              className={searching ? "loading" : ""}
               aria-label="Search contacts"
               placeholder="Search"
               type="search"
@@ -38,7 +41,7 @@ const Root = () => {
               defaultValue={q}
               onChange={(event)=>{submit(event.currentTarget.form)}}
             />
-            <div id="search-spinner" aria-hidden hidden={true} />
+            <div id="search-spinner" aria-hidden hidden={!searching} />
             <div className="sr-only" aria-live="polite"></div>
           </Form>
           <Form method="post">
